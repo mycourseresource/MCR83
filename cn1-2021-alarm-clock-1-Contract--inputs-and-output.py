4fc41372353714142b99b11ff3ed295b0e29879c
Stefan <mycourseresource@users.noreply.github.com>
2021-11-24 16:16:44 +0000

    Add files via upload

---
+++ cn1-2021-alarm-clock-1-Contract--inputs-and-output.py
@@ -0,0 +1,13 @@
+  """
+
+  :param alarmSet: true if the alarm is set to go off at specified time
+  :type alarmSet: boolean
+
+  :param alarmTime: the time when the alarm buzzer is to sound, e.g. 18:45
+  :type alarmTime: string
+
+  :returns: true if alarm is set and current time matches alarm time
+  :rtype: boolean
+
+  """
+
