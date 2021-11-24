04d2e12cc75ebbf27f9899f513b3cb10396418ce
Stefan <mycourseresource@users.noreply.github.com>
2021-11-24 16:17:30 +0000

    Add files via upload

---
+++ cn1-2021-alarm-clock-3-Header--meaningful-function-name.py
@@ -0,0 +1,14 @@
+def checkAlarm(alarmSet, alarmTime):
+  """checks if alarm is set and alarm time is reached
+
+  :param alarmSet: true if the alarm is set to go off at specified time
+  :type alarmSet: boolean
+
+  :param alarmTime: the time when the alarm buzzer is to sound, e.g. 18:45
+  :type alarmTime: string
+
+  :returns: true if alarm is set and current time matches alarm time
+  :rtype: boolean
+
+  """
+
