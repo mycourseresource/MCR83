ceb72e811522ab0947da698405674a39294c8d4c
Stefan <mycourseresource@users.noreply.github.com>
2021-11-24 16:20:13 +0000

    Add files via upload

---
+++ cn1-2021-alarm-clock-6-Definition--coding.py
@@ -0,0 +1,31 @@
+def checkAlarm(alarmSet, alarmTime):
+  """checks if alarm is set and alarm time is reached
+
+  :param alarmSet: true if the alarm is set to go off at specified time
+  :type alarmSet: boolean
+
+  :param alarmTime: the time when the alarm buzzer is to sound, e.g. 18:45
+  :type alarmTime: string
+
+  :returns: true if alarm is set and current time matches alarm time
+  :rtype: boolean
+
+  examples are based on current time of 7:00
+  >>>checkAlarm(True, '7:00')
+  True
+  >>>checkAlarm(False, '7:00')
+  False
+  >>>checkAlarm(True, '7:05')
+  False
+  >>>checkAlarm(False, '7:05')
+  False
+
+  STRATEGY: Apply conditional if else statement and logical AND operator
+
+  """
+  currentTime = '7:00' # to follow our example
+  if alarmSet==True and alarmTime==currentTime:
+    return True
+  else:
+    return False
+
